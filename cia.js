57306be94090037e3dd34769e8dea3fdf924e9d0
dandavis <rndme@users.noreply.github.com>
2016-02-25 21:25:26 -0600

    pureMutation option to ignore reducer return. heh.

--- cia.js
+++ cia.js
@@ -252,12 +252,14 @@ function CIA(reductions, state, pool) {
 
 				if(ret._blnErrorThrowing) {
 					forEach(heap, function(fn) {
-						state = (context ? fn.call(context, state, data) : fn(state, data)) || state;
+						var ret = (context ? fn.call(context, state, data) : fn(state, data));
+						if( ret!=fn._UndeF1neD && !ret._blnPureMutations) state = ret;
 					});
 				} else { // catch errors:
 					forEach(heap, function(fn) {
 						try {
-							state = (context ? fn.call(context, state, data) : fn(state, data)) || state;
+							var ret = (context ? fn.call(context, state, data) : fn(state, data)) || state;
+							if( ret!=fn._UndeF1neD && !ret._blnPureMutations) state = ret;
 						} catch(err) {
 							ret.dispatch("_ERROR_", [err, strType, data]);
 						}
@@ -311,6 +313,7 @@ CIA._blnPublishState= false; 	// add a state property to instance to allow outsi
 CIA._blnPublishReducers= false;	// add a reducer property to the instance to allow customization
 CIA._blnStrictReducers= false;	// dispatch()ing missing reducer types will throw instead of fire a _MISSING_ internal
 CIA._blnErrorThrowing= false;	//  throw on errors instead of dispatch()ing reducer errors as an _ERROR_ type internal
+CIA._blnPureMutations= false;   // optimizes for mutating pure functions by igroning reducer returns
 CIA._blnForget= false;	//  prevents keeping dispatched actions in .history. prevents .after()'s firing on adding capability
 CIA._blnDeferSubscriptions= false;// debounce state-change callbacks to reduce CPU. note: only last event of cluster will be passed
 CIA._blnDeferPeriod = 15 ;	// w/_blnDeferSubscriptions, # of ms to wait for activity to cease before firing a state-change
